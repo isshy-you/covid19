b97fc5832117d40efc2fdb8697826f31da3f2836
isshy-you <85348572+isshy-you@users.noreply.github.com>
2022-07-03 02:14:36 +0900

    ■ グラフ生成の個別関数化 ■ 都道府県別の倍率グラフ作成

--- covid19.py
+++ covid19.py
@@ -14,7 +14,7 @@ import matplotlib.dates as mdates
 import covid19_lib
 # import math
 
-def print_line(text,mode,unit,value,p_value):
+def print_line(text,mode,unit,value,p_value): #１ライン表示(未使用)
     line1=text+':'+'{:>0,.0f}'.format(value)+unit
     if mode==0:
         line2='('+'{:>+0,.0f}'.format(value-p_value)+unit+':'+'{:>0.0f}'.format(value/p_value*100)+'%)'
@@ -22,7 +22,7 @@ def print_line(text,mode,unit,value,p_value):
         line2='('+'{:>+0,.1f}'.format(value-p_value)+unit+')'
     print(line1+line2)
 
-def fwrite_line_tw(f,mode,text,unit,value,p_value):
+def fwrite_line_tw(f,mode,text,unit,value,p_value): #結果文字列Twitter用
     line1=text+':'+'{:>0,.0f}'.format(value)+unit
     if mode==0:
         line2='('+'{:>+0,.0f}'.format(value-p_value)+unit+':'+'{:>0.0f}'.format(value/p_value*100)+'%)'+'\n'
@@ -30,26 +30,26 @@ def fwrite_line_tw(f,mode,text,unit,value,p_value):
         line2='('+'{:>+0,.1f}'.format(value-p_value)+unit+')'+'\n'
     f.write(line1+line2)
 
-def fwrite_line(f,mode,text,unit,value,p_value,date):
-    if mode==0:
-        value_str = '{:>10,.0f}'.format(value)+unit
-        p_value_str = '{:>+8,.0f}'.format(value-p_value)+unit
-        p_value2_str = '{:>4.0f}'.format(value/p_value*100)
-        text_str = '{:<24}'.format(text)
+def fwrite_line(f,mode,text,unit,value,p_value,date): #結果文字列
+    if mode==0: #
+        text_str = '{:<24}'.format(text) #タイトル文字
+        value_str = '{:>10,.0f}'.format(value)+unit #人数
+        p_value_str = '{:>+8,.0f}'.format(value-p_value)+unit #前週比人数
+        p_value2_str = '{:>4.0f}'.format(value/p_value*100) #前週率
         f.write(text_str+'('+date+'):'+value_str+'(前週比'+p_value_str+':'+p_value2_str+'%)'+'\n')
-    else:
+    else: #PCR検査陽性率
+        text_str = '{:<24}'.format(text)
         value_str = '{:>9,.1f}'.format(value)+unit
         p_value_str = '{:>+7,.1f}'.format(value-p_value)+unit
-        text_str = '{:<24}'.format(text)
         f.write(text_str+'('+date+'):'+value_str+'(前週比'+p_value_str+')\n')
 
-def make_7dma(df,column):
+def make_7dma(df,column): #７日移動平均計算
     date = str(df.iloc[:,0].iloc[-1].strftime('%Y/%m/%d'))
     value = df.iloc[:,column].rolling(window=7, min_periods=1).mean().iloc[-1]
     p_value = df.iloc[:,column].rolling(window=7, min_periods=1).mean().iloc[-8]
     return(value,p_value,date)
 
-def make_tweet_text(df_list):
+def make_tweet_text(df_list): #twitter 用TXT生成
     f_tw = open('result/twitter.txt', 'w', encoding='UTF-8')
     f_tw.write('-----------------tweet text----------------------------'+'\n')
     f_tw.write('#COVID19 #新型コロナ'+'\n')
@@ -75,7 +75,7 @@ def make_tweet_text(df_list):
     fwrite_line_tw(f_tw,0,'死亡者数','',value,p_value)
     f_tw.close()
 
-def make_result_text(df_list):
+def make_result_text(df_list): #結果テキスト生成
     f = open('result/result.txt', 'w', encoding='UTF-8')
     f.write('厚生労働省オープンデータより週平均\n')
     value,p_value,date=make_7dma(df_list[pcrtest_no],1)
@@ -98,45 +98,7 @@ def make_result_text(df_list):
     fwrite_line(f,0,'死亡者数　　　　　','',value,p_value,date)
     f.close()
 
-if __name__ == "__main__":
-    print('download & coping covid19 open data from internet')
-    ap=covid19_lib.url_download()
-    ap.download_MHLW()
-    # # covid19_lib.url_download()    
-
-    print('reading covid19 data from database/*.csv')
-    load=covid19_lib.csv_load()
-    df_list,df_mag_list=load.load_MHLW_all()
-    
-    pcrtest_no = load.MHLW_names.index('pcr_tested') #0
-    inpatient_no = load.MHLW_names.index('inpatient') #1
-    newly_no = load.MHLW_names.index('newly_confirmed_cases') #2
-    severe_no = load.MHLW_names.index('severe_cases') #3
-    death_no = load.MHLW_names.index('deaths') #4
-    pcrcase_no = load.MHLW_names.index('pcr_case') #5
-    newly_100k_no = load.MHLW_names.index('newly_confirmed_cases_per_100k') #6
-
-    os.makedirs('result', exist_ok=True)
-
-    # death : convert cumulative to daily
-    for col in range(1,len(df_list[newly_no].columns),1):
-        buf2=df_list[death_no].at[0,df_list[death_no].columns[col]]
-        for ii in range(1,len(df_list[death_no]),1):
-            buf1=df_list[death_no].at[ii,df_list[death_no].columns[col]]
-            df_list[death_no].at[ii,df_list[death_no].columns[col]]=buf1-buf2
-            buf2=buf1
-        df_list[death_no].at[0,df_list[death_no].columns[col]]=0
-
-    #calculate and set xmin,xmax,ymin,ymax
-    xmin = datetime.datetime.strptime('2021-07-01', '%Y-%m-%d')
-    # xmax = datetime.datetime.strptime('2100-01-01', '%Y-%m-%d')
-    # for ii,dname in enumerate(load.MHLW_fnames):
-    #     xtmp = np.max([df_list[ii].iloc[:,0]])
-    #     xmax = np.min([xtmp,xmax])
-    # print('from:',xmin,' to:',xmax)
-    ymin = 1
-    ymax = 1_000_000
-
+def make_graph_MHLW_NCR():
     # newly_confirmed/pcrtest ratio graph
     print('making covid19 graph : result/covid19_MHLW_ncr.png')
     fig = plt.figure(1,figsize=(6,6))
@@ -167,7 +129,10 @@ if __name__ == "__main__":
     plt.cla()
     plt.clf()
 
+def make_graph_MHLW_ALL():
     # All Graph
+    fig = plt.figure(1,figsize=(6,6))
+    axes = fig.add_subplot(111)
     print('making covid19 graph : result/covid19_MHLW_All_7dMA')
     plt.title('COVID-19 from MHLW Open Data (7days Moving Average)')
     xmax = datetime.datetime.strptime('2100-01-01', '%Y-%m-%d')
@@ -193,6 +158,9 @@ if __name__ == "__main__":
     plt.clf()
     # plt.close()
 
+def make_graph_MHLW_MAG():
+    fig = plt.figure(1,figsize=(6,6))
+    axes = fig.add_subplot(111)
     print('making covid19 magnitude graph : result/covid19_MHLW_All_7dMA_mag')
     plt.title('COVID-19 from MHLW Open Data (7days Moving Average)')
     xmax = datetime.datetime.strptime('2100-01-01', '%Y-%m-%d')
@@ -219,6 +187,7 @@ if __name__ == "__main__":
     plt.clf()
     plt.close()
 
+def make_graph_MHLW_PREF():
     # prefecture graph
     print('making covid19 graph for each prefecture')
     xmax = datetime.datetime.strptime('2100-01-01', '%Y-%m-%d')
@@ -255,43 +224,52 @@ if __name__ == "__main__":
         plt.close() 
     print('\r','                                     ')
 
-    # prefecture graph
-    print('making covid19 magnitude graph for each prefecture')
-    # xmin = datetime.datetime.strptime('2022-04-01', '%Y-%m-%d')
-    xmax = datetime.datetime.strptime('2100-01-01', '%Y-%m-%d')
-    for col in range(1,len(df_mag_list[newly_no].columns),1):
-        fig = plt.figure(1,figsize=(6,6))
-        axes = fig.add_subplot(111)
-        # print('\r','-',df_mag_list[newly_no].columns[col],'           ',end="")
-        plt.title(df_mag_list[newly_no].columns[col]+':COVID-19 from MHLW Open Data (7days Moving Average)')
-        for jj in [newly_no,inpatient_no]:
-            if jj==inpatient_no:
-                plt.plot(df_mag_list[jj].iloc[:,0],df_mag_list[jj].iloc[:,1+(col-1)*3].rolling(window=7, min_periods=1).mean(),label=load.MHLW_names[jj])
-            else:
-                plt.plot(df_mag_list[jj].iloc[:,0],df_mag_list[jj].iloc[:,col].rolling(window=7, min_periods=1).mean(),label=load.MHLW_names[jj])
-            xtmp = np.max([df_mag_list[jj].iloc[:,0]])
-            xmax = np.min([xtmp,xmax])
-        plt.xlim(xmin,xmax)
-        plt.ylim(0.5,5)
-        plt.yscale("log")
-        plt.legend()
-        plt.tick_params(axis='x', rotation=90)
-        axes.xaxis.set_major_formatter(mdates.DateFormatter('%y/%m/%d')) # yy/mm/dd
-        axes.xaxis.set_major_locator(mdates.DayLocator(interval=7)) # by 1 week
-        # axes.xaxis.set_major_locator(mdates.MonthLocator(interval=1)) # by 1 month
-        # plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7)) # by 1 week
-        plt.grid()
-        # plt.gcf().autofmt_xdate()
-        plt.tight_layout()
-        # plt.show()
-        fname='result/covid19_MHLW_{:02d}'.format(col-1)+df_mag_list[newly_no].columns[col]+'_mag'
-        print('\r','-'+fname+'.png                   ',end="")
-        fig.savefig(fname, bbox_inches="tight", pad_inches=0.05)
-        plt.cla()
-        plt.clf()
-        plt.close() 
+def make_graph_MHLW_PREF():
+    # prefecture text
+    print('making covid19 magnitude list for each prefecture')
+    pref_mag_num=[]
+    pref_mag_val=[]
+    pref_mag_name=[]
+    pref_mag_color=[]
+    fname='result/covid19_MHLW_Pref_Mag.txt'
+    with open(fname, mode='w') as f:
+        for col in range(1,len(df_mag_list[newly_no].columns),1):
+            # print('\r','-',df_mag_list[newly_no].columns[col],'           ',end="")
+            # for jj in [newly_no,inpatient_no]:
+            jj = newly_no
+            # if jj==inpatient_no:
+            #     val = df_mag_list[jj].iloc[:,1+(col-1)*3].rolling(window=7, min_periods=1).mean()
+            # else:
+            val = df_mag_list[jj].iloc[:,col].rolling(window=7, min_periods=1).mean()
+            f.write(df_list[newly_no].columns[col]+','+str(val[len(val)-1])+'\n')
+            pref_mag_num.append(int(col))
+            pref_mag_name.append(df_list[newly_no].columns[col])
+            pref_mag_val.append(float(val[len(val)-1]))
+            print('\r','-'+fname+'                   ',end="")
     print('\r','                                       ')
+    for kk,val in enumerate(pref_mag_val):
+        if val < 1:
+            pref_mag_color.append('blue')
+        else:
+            pref_mag_color.append('red')
+        pref_mag_val[kk]=(val-1)*100
 
+    fig = plt.figure(1,figsize=(12,6))
+    axes = fig.add_subplot(111)
+    axes.bar(pref_mag_num,pref_mag_val,tick_label=pref_mag_name,color=pref_mag_color,align='center',log=False)
+    axes.tick_params(axis='x', rotation=90)
+    axes.set_axisbelow(True)
+    # axes.grid(visible=True, which="major", color="#ababab", linestyle="-", axis="y")
+    # plt.gird()
+    axes.grid(which="major",alpha=0.6)
+    axes.grid(which="minor",alpha=0.3)
+    axes.set_title("COVID-19 from MHLW Open Data \n newly confirmed weekly increase rate \n (7days Moving Average)")
+    axes.set_ylabel('increase rate [+/-%]')
+    # plt.tight_layout()
+    fname='result/covid19_MHLW_Pref_Mag.png'
+    fig.savefig(fname, bbox_inches="tight", pad_inches=0.05)
+
+def make_graph_MHLW_100k():
     # 10k newly graph by pickuped Prefectures
     print('making covid19 graph : result/covid19_100k_MHLW.png')
     pref_list = ['Hokkaido','Tokyo','Aichi','Osaka','Fukuoka','Okinawa']
@@ -315,36 +293,55 @@ if __name__ == "__main__":
     plt.clf()
     plt.close()
 
-    make_tweet_text(df_list)
-    make_result_text(df_list)
-    # f = open('result/result.txt', 'w', encoding='UTF-8')
-    # f_tw = open('result/twitter.txt', 'w', encoding='UTF-8')
-    # f_tw.write('#COVID19 #新型コロナ')
-    # f.write('厚生労働省オープンデータより週平均\n')
-    # f_tw.write('-----------------tweet text----------------------------')
-    # value,p_value,date=make_7dma(df_list[pcrtest_no],1)
-    # f_tw.write('厚生労働省データより週平均('+date+')')
-    # f_tw.write('()内前週比')
-    # fwrite_line_tw(f_tw,'PCR検査数','',value,p_value)
-    # fwrite_line(f,'ＰＣＲ検査数　　　','',value,p_value,date)
-    # pcrtest=value
-    # p_pcrtest=p_value
-    # d_pcrtest=date
-    # value,p_value,date=make_7dma(df_list[newly_no],1)
-    # fwrite_line_tw(f_tw,0,'新規陽性者数','',value,p_value)
-    # fwrite_line(f,'新規陽性者数　　　','',value,p_value,date)
-    # newly=value
-    # p_newly=p_value
-    # d_newly=date
-    # if d_pcrtest==d_newly:
-    #     fwrite_line_tw(f_tw,1,'PCR検査陽性率','%',newly/pcrtest*100,p_newly/p_pcrtest*100)
-    #     fwrite_line1(f,'ＰＣＲ検査陽性率　','%',newly/pcrtest*100,p_newly/p_pcrtest*100,date)
-    # value,p_value,date=make_7dma(df_list[inpatient_no],1)
-    # fwrite_line_tw(f_tw,0,'入院治療を要する者','',value,p_value)
-    # fwrite_line(f,'入院治療を要する者','',value,p_value,date)
-    # value,p_value,date=make_7dma(df_list[severe_no]   ,1)
-    # fwrite_line_tw(f_tw,0,'重症者数','',value,p_value)
-    # fwrite_line(f,'重症者数　　　　　','',value,p_value,date)
-    # value,p_value,date=make_7dma(df_list[death_no]    ,1)
-    # fwrite_line_tw(f_tw,0,'死亡者数','',value,p_value)
-    # fwrite_line(f,'死亡者数　　　　　','',value,p_value,date)
+
+if __name__ == "__main__":
+    # define SWITCH
+    DOWNLOAD = 0
+
+    print('download & coping covid19 open data from internet')
+    ap=covid19_lib.url_download()
+    if DOWNLOAD==1 : ap.download_MHLW()
+
+    print('reading covid19 data from database/*.csv')
+    load=covid19_lib.csv_load()
+    df_list,df_mag_list=load.load_MHLW_all()
+    
+    pcrtest_no = load.MHLW_names.index('pcr_tested') #0
+    inpatient_no = load.MHLW_names.index('inpatient') #1
+    newly_no = load.MHLW_names.index('newly_confirmed_cases') #2
+    severe_no = load.MHLW_names.index('severe_cases') #3
+    death_no = load.MHLW_names.index('deaths') #4
+    pcrcase_no = load.MHLW_names.index('pcr_case') #5
+    newly_100k_no = load.MHLW_names.index('newly_confirmed_cases_per_100k') #6
+
+    os.makedirs('result', exist_ok=True)
+
+    # death : convert cumulative to daily
+    for col in range(1,len(df_list[newly_no].columns),1):
+        buf2=df_list[death_no].at[0,df_list[death_no].columns[col]]
+        for ii in range(1,len(df_list[death_no]),1):
+            buf1=df_list[death_no].at[ii,df_list[death_no].columns[col]]
+            df_list[death_no].at[ii,df_list[death_no].columns[col]]=buf1-buf2
+            buf2=buf1
+        df_list[death_no].at[0,df_list[death_no].columns[col]]=0
+
+    #calculate and set xmin,xmax,ymin,ymax
+    xmin = datetime.datetime.strptime('2021-07-01', '%Y-%m-%d')
+    # xmax = datetime.datetime.strptime('2100-01-01', '%Y-%m-%d')
+    # for ii,dname in enumerate(load.MHLW_fnames):
+    #     xtmp = np.max([df_list[ii].iloc[:,0]])
+    #     xmax = np.min([xtmp,xmax])
+    # print('from:',xmin,' to:',xmax)
+    ymin = 1
+    ymax = 1_000_000
+
+    # make_graph_MHLW_NCR()
+    # make_graph_MHLW_ALL()
+    # make_graph_MHLW_MAG()
+    # make_graph_MHLW_PREF()
+    make_graph_MHLW_PREF()
+    # make_graph_MHLW_100k()
+    # make_tweet_text(df_list)
+    # make_result_text(df_list)
+
+
